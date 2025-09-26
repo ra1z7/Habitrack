b9c9480b6572c9e35f3cfcabd88ccc62fb44cd79
ra1z <purnamaanrai7@gmail.com>
2025-09-26 06:26:21 +0545

    Initial Commit

---
+++ Habitrack/HabitrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitrackApp.swift
+//  Habitrack
+//
+//  Created by Purnaman Rai (College) on 26/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
