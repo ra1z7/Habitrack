981198ffdaaf0950f6be8e507bc488775a141b37
ra1z <purnamaanrai7@gmail.com>
2025-09-27 15:27:31 +0545

    Make habit detail view look more beautiful [Not Complete]

--- Habitrack/HabitDetailView.swift
+++ Habitrack/HabitDetailView.swift
@@ -10,18 +10,76 @@ import SwiftUI
 struct HabitDetailView: View {
     var habit: Habit
     
+    var randomColor: Color {
+        let allColors: [Color] = [
+            .red,
+            .orange,
+            .yellow,
+            .green,
+            .mint,
+            .teal,
+            .cyan,
+            .blue,
+            .indigo,
+            .purple,
+            .pink,
+            .brown,
+            .gray
+        ]
+        
+        return allColors.randomElement()!
+    }
+    
     var body: some View {
-        VStack {
-            Text(habit.title)
-                .font(.title)
+        VStack(spacing: 20) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 60)
+                    .fill(randomColor.gradient.opacity(0.5))
+                    .ignoresSafeArea()
+                    .frame(width: 410, height: 40)
+                Text(habit.title)
+                    .font(.title.bold())
+                    .foregroundStyle(.white)
+                    .offset(y: -52)
+            }
+            
             Text(habit.description)
                 .font(.title3)
-            Text("Completion: \(habit.completition)")
+                .frame(width: .infinity, height: 200, alignment: .top)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(.black.opacity(0.2), lineWidth: 2)
+                }
+                .padding()
             
-            Button("Completed") {
-                habit.incrementCompletionCount()
+            HStack {
+                Text("\(habit.completition)")
+                    .frame(width: 120, height: 50)
+                    .shadow(radius: 10)
+                    .background(.blue.gradient.opacity(0.4))
+                    .foregroundStyle(.white)
+                    .clipShape(.rect(cornerRadius: 15))
+                    .fontWeight(.bold)
+                
+                Button {
+                    habit.incrementCompletionCount()
+                } label: {
+                    HStack {
+                        Text("Completed")
+                        Image(systemName: "checkmark")
+                    }
+                    .frame(width: 220, height: 50)
+                    .shadow(radius: 10)
+                    .background(.green.gradient)
+                    .foregroundStyle(.white)
+                    .clipShape(.rect(cornerRadius: 15))
+                    .fontWeight(.bold)
+                }
             }
+            
+            Spacer()
         }
+
     }
     
     init(for habit: Habit) {
